63a324934f105afce502ac6f0ea920dd6826cee8
Jeeva <jeevabaskaran328@gmail.com>
2022-03-12 16:46:38 +0530

    03

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Title from "./components/functional/Title";
+import Menu from "./components/functional/Menu";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  var client = new QueryClient();
+
+    return ( 
+      <QueryClientProvider client={client}>
+
+      <BrowserRouter >
+      <Title/>
+      <Menu/>
+
+        <Routes>
+
+          <Route>
+
+          </Route>
+
+
+        </Routes> 
+        
+        </BrowserRouter>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
